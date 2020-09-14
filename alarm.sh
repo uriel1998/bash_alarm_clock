063cc19f6d7b7eaefef54b7a6e5c0682f615604b
Steven Saus <steven@stevesaus.com>
2020-09-14 18:53:16 -0400

    Fix for audio from cron

--- alarm.sh
+++ alarm.sh
@@ -4,6 +4,8 @@
 #https://friedcpu.wordpress.com/2007/07/17/why-arent-you-using-ionice-yet/
 ionice -c3 -p$$
 
+
+export XDG_RUNTIME_DIR="/run/user/1000"
 #https://web.archive.org/web/20080617195246/http://grimthing.com/archives/2004/01/23/cron-mp3-alarm-clock/
 
 Weekend="False"
@@ -26,23 +28,25 @@ else
     NowTime="$1"
 fi
 
+#mplayer commandline tweaked via https://itectec.com/ubuntu/ubuntu-help-using-crontab-to-play-a-sound/
+
 if [ "$Weekend" = "False" ]; then
     case "$NowTime" in
             "0600")
-                /usr/bin/mplayer -really-quiet "/home/steven/music/antarctica/01-simon slator-antarctica.mp3" &
+                /usr/bin/mplayer -ao pulse -volume 100 -really-quiet "/home/steven/documents/background_tunes/relax/01-simon slator-antarctica.mp3" &
                 echo $! >/tmp/alarm_1.pid
             ;;
             "0630") 
                 /home/steven/scripts/briefing.sh
-                /usr/bin/mplayer -really-quiet -playlist "/home/steven/briefing/play.m3u" &
+                /usr/bin/mplayer -ao pulse -volume 100 -really-quiet -playlist "/home/steven/briefing/play.m3u" &
                 echo $! >/tmp/alarm_2.pid
             ;;
             "0655")     
-                /usr/bin/mplayer -really-quiet  -loop 0 "/home/steven/music/the essential nilsson/01-harry nilsson-gotta get up.mp3" &
+                /usr/bin/mplayer -ao pulse -volume 100 -really-quiet -loop 5 "/home/steven/music/the essential nilsson/01-harry nilsson-gotta get up.mp3" &
                 echo $! >/tmp/alarm_3.pid
             ;;
             "0700") 
-                /usr/bin/mplayer -really-quiet -loop 0 "/home/steven/music/goru bihu song/rainbow trip-goru bihu song.mp3" &
+                /usr/bin/mplayer -ao pulse -volume 100 -really-quiet -loop 5 "/home/steven/music/goru bihu song/rainbow trip-goru bihu song.mp3" &
                 echo $! >/tmp/alarm_4.pid
             ;;
             "-km")
