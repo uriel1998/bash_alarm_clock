9a389f2349b9b99879f387e4a057d20a885e4027
Steven Saus <steven@stevesaus.com>
2022-05-25 19:07:28 -0500

    starting over with alarm, because ugh.

--- alarm.sh
+++ alarm.sh
@@ -36,16 +36,16 @@ if [ "$Weekend" = "False" ]; then
                 /usr/bin/mplayer -ao pulse -volume 100 -really-quiet "/home/steven/documents/background_tunes/relax/01-simon slator-antarctica.mp3" &
                 echo $! >/tmp/alarm_1.pid
             ;;
-            "0630") 
+            "0545") 
                 /home/steven/scripts/briefing.sh
                 /usr/bin/mplayer -ao pulse -volume 100 -really-quiet -playlist "/home/steven/briefing/play.m3u" &
                 echo $! >/tmp/alarm_2.pid
             ;;
-            "0655")     
+            "0650")     
                 /usr/bin/mplayer -ao pulse -volume 100 -really-quiet -loop 5 "/home/steven/music/the essential nilsson/01-harry nilsson-gotta get up.mp3" &
                 echo $! >/tmp/alarm_3.pid
             ;;
-            "0700") 
+            "0655") 
                 /usr/bin/mplayer -ao pulse -volume 100 -really-quiet -loop 5 "/home/steven/music/goru bihu song/rainbow trip-goru bihu song.mp3" &
                 echo $! >/tmp/alarm_4.pid
             ;;
